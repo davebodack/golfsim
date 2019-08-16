5df916c69f6d5d69b666113a71d3fcc199260269
David Bodack <32307099+davebodack@users.noreply.github.com>
2019-08-15 21:31:26 -0400

    Add files via upload
    Tweaked the Ryder Cup mode to allow the simulation of a President's Cup too. Also made sure there wouldn't be bugs in the International mode that would make there be not enough points distributed.

--- golfsim.cpp
+++ golfsim.cpp
@@ -159,29 +159,26 @@ Player* run_match(Player golfer1, Player golfer2, int nummatches, bool printflag
 		for (int i = 1; i < 19; i++) {
 			holeresult = determine_hole_winner(golfer1, golfer2);
 			numholesleft--;
+
+			if (holeresult == 1) {
+				roundsum--;
+			} else if (holeresult == 3) {
+				roundsum++;
+			}
+
 			if ((abs(roundsum) > numholesleft) && (victoryflag == false)) {
+				victoryflag = true;
 				if (roundsum < 0) {
-					victoryflag = true;
 					golfer1.nummatcheswon++;
 					if ((nummatches == 1) && (printflag == true)) {
 						cout << golfer1.name << " defeats " << golfer2.name << " " << abs(roundsum) << " & " << numholesleft << "!\n";
 					}
 				}
 				else {
-					victoryflag = true;
 					golfer2.nummatcheswon++;
 					if ((nummatches == 1) && (printflag == true)) {
 						cout << golfer2.name << " defeats " << golfer1.name << " " << abs(roundsum) << " & " << numholesleft << "!\n";
 					}	
-				}
-			}
-
-			else {
-				if (holeresult == 1) {
-					roundsum--;
-				}
-				else if (holeresult == 3) {
-					roundsum++;
 				}
 			}
 		}
@@ -280,10 +277,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	string input;
-	cout << "If you'd like to simulate a full career, type 'career'. If you'd like to simulate a match play tournament, type 'match play'. If you'd like to draft a golfer and play a match against someone, type 'one on one'. If you'd like to simulate an all-time Ryder Cup, type 'Ryder Cup'.\n"; 
+	cout << "If you'd like to simulate a full career, type 'career'. If you'd like to simulate a match play tournament, type 'match play'. If you'd like to draft a golfer and play a match against someone, type 'one on one'. If you'd like to simulate an all-time Ryder Cup or Presidents Cup, type 'International'.\n"; 
 	getline(cin, input);
 
-	while ((input != "Career") && (input != "career") && (input != "match play") && (input != "Match play") && (input != "Match Play") && (input != "one on one") && (input != "1 on 1") && (input != "One on One") && (input != "One On One") && (input != "Ryder Cup") && (input != "Ryder cup") && (input != "ryder cup") && (input != "rydercup")) {
+	while ((input != "Career") && (input != "career") && (input != "match play") && (input != "Match play") && (input != "Match Play") && (input != "one on one") && (input != "1 on 1") && (input != "One on One") && (input != "One On One") && (input != "Ryder Cup") && (input != "International") && (input != "international") && (input != "intl")) {
 		cout << "Oops! You entered an invalid input. Try again: to simulate a full career, type 'career'. To simulate match play, type 'match play'. To pit two golfers against each other in a one on one match, type 'one on one'. If you'd like to simulate an all-time Ryder Cup, type 'Ryder Cup'.\n";
 		getline(cin, input);
 	}
@@ -631,37 +628,62 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	else if ((input == "Ryder Cup") || (input == "Ryder cup") || (input == "ryder cup") || (input == "rydercup")) {
+	else if ((input == "International") || (input == "international") || (input == "intl")) {
 		
 		Player americans[12];
-		Player europeans[12];
+		Player internationals[12];
 		int americancounter = 0;
-		int europeancounter = 0;
+		int internationalcounter = 0;
 		double american_points;
-		double european_points;
+		double international_points;
 		int americanrydercupswon = 0;
-		int europeanrydercupswon = 0;
+		int internationalrydercupswon = 0;
+
+		string internationalinput;
+		char internationaltype;
+
+		cout << "\nIf you'd like to simulate a Ryder Cup, type 'Ryder Cup'. If you'd like to simulate a President's Cup, type 'President's Cup'.\n";
+		cin >> internationalinput;
+
+		while ((internationalinput != "Ryder Cup") && (internationalinput != "President's Cup") && (internationalinput != "ryder cup") && (internationalinput != "president's cup") && (internationalinput != "presidents cup") && (internationalinput != "Ryder") && (internationalinput != "ryder") && (internationalinput != "Presidents") && (internationalinput != "presidents")) {
+			cout << "Invalid input entered, please select either the Ryder Cup by typing 'Ryder Cup' or the President's Cup by typing 'President's Cup'.\n";
+			cin >> internationalinput;
+		}
+
+		if ((internationalinput == "Ryder Cup") || (internationalinput == "ryder cup") || (internationalinput == "Ryder") || (internationalinput == "ryder")) {
+			internationaltype = 'E';
+		} else {
+			internationaltype = 'R';
+		}
 
 		for (int i = 0; i < golfernum; i++) {
 			if ((golfers[i].nationality == 'A') && (americancounter < 12)) {
 				americans[americancounter] = golfers[i];
 				americancounter++;
-			} else if ((golfers[i].nationality == 'E') && (europeancounter < 12)) {
-				europeans[europeancounter] = golfers[i];
-				europeancounter++;
+			} else if ((golfers[i].nationality == internationaltype) && (internationalcounter < 12)) {
+				internationals[internationalcounter] = golfers[i];
+				internationalcounter++;
 			}
 		}
 
 		srand(time(0));
 		shuffle(americans);
-		shuffle(europeans);
+		shuffle(internationals);
 		
 		int numrydercups;
-		cout << "\nHow many Ryder Cups would you like to simulate? ";
+		if (internationaltype == 'E') {
+			cout << "\nHow many Ryder Cups would you like to simulate?\n";
+		} else {
+			cout << "\nHow many President's Cups would you like to simulate?\n";
+		}
 		cin >> numrydercups;
 
-		while ((!cin) || (numrydercups < 1)) {
-			cout << "Invalid number of Ryder Cups entered. Please enter a valid number of Ryder Cups to be simulated:\n";
+		while ((!cin) && (numrydercups < 1)) {
+			if (internationaltype == 'E') {
+				cout << "Invalid number of Ryder Cups entered. Please enter a valid number of Ryder Cups to be simulated:\n";
+			} else {
+				cout << "Invalid number of President's Cups entered. Please enter a valid number of President's Cups to be simulated:\n";
+			}
 			cin >> numrydercups;
 		}
 
@@ -676,125 +698,149 @@ int main(int argc, char* argv[]) {
 				americans[x].rydercuppoints = 0;
 				americans[x].nummatcheswon = 0;
 				americans[x].nummatchestied = 0;
-				europeans[x].rydercuppoints = 0;
-				europeans[x].nummatcheswon = 0;
-				europeans[x].nummatchestied = 0;
+				internationals[x].rydercuppoints = 0;
+				internationals[x].nummatcheswon = 0;
+				internationals[x].nummatchestied = 0;
 			}
 
 			for (int i = 0; i < 4; i++) {
 
 				american_points = 0;
-				european_points = 0;
+				international_points = 0;
 
 				for (int j = 0; j < 8; j += 2) {
-					int fourball_result = determine_foursome_winner(americans[j], americans[j+1], europeans[j], europeans[j+1]);
+					int fourball_result = determine_foursome_winner(americans[j], americans[j+1], internationals[j], internationals[j+1]);
 					americans[j].nummatchesplayed++;
 					americans[j+1].nummatchesplayed++;
-					europeans[j].nummatchesplayed++;
-					europeans[j+1].nummatchesplayed++;
+					internationals[j].nummatchesplayed++;
+					internationals[j+1].nummatchesplayed++;
 					if (fourball_result == 1) {
 						americans[j].rydercuppoints++;
 						americans[j+1].rydercuppoints++;
 						if (numrydercups == 1) {
-							cout << americans[j].name << " and " << americans[j+1].name << " defeat " << europeans[j].name << " and " << europeans[j+1].name << ".\n";
+							cout << americans[j].name << " and " << americans[j+1].name << " defeat " << internationals[j].name << " and " << internationals[j+1].name << ".\n";
 						}
 					} else if (fourball_result == 2) {
 						americans[j].rydercuppoints += 0.5;
-						europeans[j].rydercuppoints += 0.5;
+						internationals[j].rydercuppoints += 0.5;
 						if (numrydercups == 1) {
-							cout << americans[j].name << " and " << americans[j+1].name << " tie " << europeans[j].name << " and " << europeans[j+1].name << ".\n";
+							cout << americans[j].name << " and " << americans[j+1].name << " tie " << internationals[j].name << " and " << internationals[j+1].name << ".\n";
 						}
 					} else {
-						europeans[j].rydercuppoints++;
-						europeans[j+1].rydercuppoints++;
+						internationals[j].rydercuppoints++;
+						internationals[j+1].rydercuppoints++;
 						if (numrydercups == 1) {
-							cout << europeans[j].name << " and " << europeans[j+1].name << " defeat " << americans[j].name << " and " << americans[j+1].name << ".\n";
+							cout << internationals[j].name << " and " << internationals[j+1].name << " defeat " << americans[j].name << " and " << americans[j+1].name << ".\n";
 						}
 					}
 				}
 
 				for (int i = 0; i < 12; i++) {
 					american_points += americans[i].rydercuppoints;
-					european_points += europeans[i].rydercuppoints;
+					international_points += internationals[i].rydercuppoints;
 				}
 
 				cout << '\n';
 
 				if ((i == 1) || (i == 3)) {
 					american_points /= 2;
-					european_points /= 2;
+					international_points /= 2;
 
 					if (numrydercups == 1) {
-						if (american_points > european_points) {
-							cout << "After Day " << ((i / 2) + 1) << ", the Americans lead the Europeans, " << american_points << "-" << european_points << "!\n\n";
-						} else if (american_points == european_points) {
-							cout << "After Day " << ((i / 2) + 1) << ", the Americans and the Europeans are tied at " << american_points << "!\n\n"; 
+						if (american_points > international_points) {
+							if (internationaltype == 'E') {
+								cout << "After Day " << ((i / 2) + 1) << ", the Americans lead the Europeans, " << american_points << "-" << international_points << "!\n\n";
+							} else {
+								cout << "After Day " << ((i / 2) + 1) << ", the Americans lead the Internationals, " << american_points << "-" << international_points << "!\n\n";
+							}
+						} else if (american_points == international_points) {
+							if (internationaltype == 'E') {
+								cout << "After Day " << ((i / 2) + 1) << ", the Americans and the Europeans are tied at " << american_points << "!\n\n"; 
+							} else {
+								cout << "After Day " << ((i / 2) + 1) << ", the Americans and the Internationals are tied at " << american_points << "!\n\n"; 
+							}
 						} else {
-							cout << "After Day " << ((i / 2) + 1) << ", the Europeans lead the Americans, " << european_points << "-" << american_points << "!\n\n";
+							if (internationaltype == 'E') {
+								cout << "After Day " << ((i / 2) + 1) << ", the Europeans lead the Americans, " << international_points << "-" << american_points << "!\n\n";
+							} else {
+								cout << "After Day " << ((i / 2) + 1) << ", the Internationals lead the Americans, " << international_points << "-" << american_points << "!\n\n";
+							}
 						}
 					}
 				}
 
 				shuffle(americans);
-				shuffle(europeans);
+				shuffle(internationals);
 			}
 
 			shuffle(americans);
-			shuffle(europeans);
+			shuffle(internationals);
 
 			for (int i = 0; i < 12; i++) {
 				americans[i].nummatcheswon = 0;
-				europeans[i].nummatcheswon = 0;
+				internationals[i].nummatcheswon = 0;
 				americans[i].nummatchestied = 0;
-				europeans[i].nummatchestied = 0;
+				internationals[i].nummatchestied = 0;
 
 				Player *rydercuppointer;
-				rydercuppointer = run_match(americans[i], europeans[i],1, rydercupprintflag);
-				
+				rydercuppointer = run_match(americans[i], internationals[i],1, rydercupprintflag);
+
 				americans[i] = rydercuppointer[0];
-				europeans[i] = rydercuppointer[1];
+				internationals[i] = rydercuppointer[1];
 				americans[i].nummatchesplayed++;
-				europeans[i].nummatchesplayed++;
+				internationals[i].nummatchesplayed++;
 
 				if (americans[i].nummatchestied == 1) {
 					americans[i].rydercuppoints += 0.5;
 					american_points += 0.5;
-					europeans[i].rydercuppoints += 0.5;
-					european_points += 0.5;
+					internationals[i].rydercuppoints += 0.5;
+					international_points += 0.5;
 				} else {
 					americans[i].rydercuppoints += americans[i].nummatcheswon;
 					american_points += americans[i].nummatcheswon;
-					europeans[i].rydercuppoints += europeans[i].nummatcheswon;
-					european_points += europeans[i].nummatcheswon;
+					internationals[i].rydercuppoints += internationals[i].nummatcheswon;
+					international_points += internationals[i].nummatcheswon;
 				}
 
 				americans[i].totalrydercuppoints += americans[i].rydercuppoints;
-				europeans[i].totalrydercuppoints += europeans[i].rydercuppoints;
+				internationals[i].totalrydercuppoints += internationals[i].rydercuppoints;
 			}
 
 			ryder_cup_sort(americans);
-			ryder_cup_sort(europeans);
+			ryder_cup_sort(internationals);
 
 			if (numrydercups == 1) {
 				cout << "\nFinal points tally:\n";
 				for (int i = 0; i < 12; i++) {
 					cout << americans[i].name << ": " << americans[i].nummatchesplayed << " matches, " << americans[i].rydercuppoints  << " points";
-					for (int j = 0; j < 15 - americans[i].name.size(); j++) {
+					for (int j = 0; j < 17 - americans[i].name.size(); j++) {
 						cout << " ";
 					}
-					cout << "| " << europeans[i].name << ": " << europeans[i].nummatchesplayed << " matches, " << europeans[i].rydercuppoints << " points\n";
+					cout << "| " << internationals[i].name << ": " << internationals[i].nummatchesplayed << " matches, " << internationals[i].rydercuppoints << " points\n";
 				}
 			}
 
 
-			if (american_points > european_points) {
+			if (american_points > international_points) {
 				americanrydercupswon++;
-				cout << "\nThe Americans emerge victorious at the Ryder Cup, defeating the Europeans " << american_points << " to " << european_points << "!\n\n";
-			} else if (american_points == european_points) {
-				cout << "\nThe Americans and the Europeans end up deadlocked in a tie, 14 to 14!\n\n";
+				if (internationaltype == 'E') {
+					cout << "\nThe Americans emerge victorious at the Ryder Cup, defeating the Europeans " << american_points << " to " << international_points << "!\n";
+				} else {
+					cout << "\nThe Americans emerge victorious at the Ryder Cup, defeating the Internationals " << american_points << " to " << international_points << "!\n";
+				}
+			} else if (american_points == international_points) {
+				if (internationaltype == 'E') {
+					cout << "\nThe Americans and the Europeans end up deadlocked in a tie, 14 to 14!\n";
+				} else {
+					cout << "\nThe Americans and the Internationals end up deadlocked in a tie, 14 to 14!\n";
+				}
 			} else {
-				europeanrydercupswon++;
-				cout << "\nThe Europeans emerge victorious at the Ryder Cup, defeating the Americans " << european_points << " to " << american_points << "!\n\n";
+				internationalrydercupswon++;
+				if (internationaltype == 'E') {
+					cout << "\nThe Europeans emerge victorious at the Ryder Cup, defeating the Americans " << international_points << " to " << american_points << "!\n";
+				} else {
+					cout << "\nThe Internationals emerge victorious at the Ryder Cup, defeating the Americans " << international_points << " to " << american_points << "!\n";
+				}
 			}
 		}
 
@@ -805,13 +851,18 @@ int main(int argc, char* argv[]) {
 				for (int j = 0; j < 17 - americans[i].name.size(); j++) {
 					cout << " ";
 				}
-				cout << "| " << europeans[i].name << ": " << europeans[i].nummatchesplayed << " matches, " << europeans[i].totalrydercuppoints << " points\n";
+				cout << "| " << internationals[i].name << ": " << internationals[i].nummatchesplayed << " matches, " << internationals[i].totalrydercuppoints << " points\n";
 			}
 			if (numrydercups > 1) {
-				cout << "\nThe Americans win " << americanrydercupswon << " Ryder Cups and the Europeans win " << europeanrydercupswon << " Ryder Cups, with " << (numrydercups - americanrydercupswon - europeanrydercupswon) << " Ryder Cups tied between them.\n\n";
+				if (internationaltype == 'E') {
+					cout << "\nThe Americans win " << americanrydercupswon << " Ryder Cups and the Europeans win " << internationalrydercupswon << " Ryder Cups, with " << (numrydercups - americanrydercupswon - internationalrydercupswon) << " Ryder Cups tied between them.\n\n";
+				} else {
+					cout << "\nThe Americans win " << americanrydercupswon << " Ryder Cups and the Internationals win " << internationalrydercupswon << " Ryder Cups, with " << (numrydercups - americanrydercupswon - internationalrydercupswon) << " Ryder Cups tied between them.\n\n";
+				}
 			}
 		}
 	}
 
 	infile.close();
 }
+
